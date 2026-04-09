aeb2584b6e57a65e07f77196aedff020e2adce54
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-04-09 10:04:43 +0100

    Componente hero da home criado

---
+++ src/components/home/Hero.jsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, EffectFade, Pagination, Navigation } from "swiper/modules";
+import Link from "next/link";
+import Image from "next/image";
+
+// Estilos do Swiper
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import heroBg1 from "@/assets/img/hero1.jpg"; 
+import heroBg2 from "@/assets/img/hero2.jpg";
+import heroBg3 from "@/assets/img/hero3.jpg";
+
+export default function Hero() {
+  return (
+    <section>
+    </section>
+  );
+}
