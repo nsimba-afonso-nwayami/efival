739ad62a5a324774fcdc3c6e04e6c18bbcafad60
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-04-09 15:56:41 +0100

    Página home do site feita

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,10 @@
 import Hero from "@/components/home/Hero";
+import Sobre from "@/components/home/Sobre";
+import Servicos from "@/components/home/Servicos";
+import Projetos from "@/components/home/Projetos";
+import Diferenciais from "@/components/home/Diferenciais";
+import Contato from "@/components/home/Contato";
+import Cta from "@/components/home/Cta";
 
 // Configuração de Metadata (SEO Principal)
 export const metadata = {
@@ -26,6 +32,12 @@ export default function Home() {
   return (
     <>
       <Hero />
+      <Sobre />
+      <Servicos />
+      <Projetos />
+      <Diferenciais />
+      <Contato />
+      <Cta />
     </>
   );
 }
