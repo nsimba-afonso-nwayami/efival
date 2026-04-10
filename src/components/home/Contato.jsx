93dccf78e5a440ca7553542163a69aa8698c594f
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-04-10 12:54:27 +0100

    Endereço do mapa atualizado

--- src/components/home/Contato.jsx
+++ src/components/home/Contato.jsx
@@ -111,7 +111,7 @@ export default function Contato() {
           transition={{ duration: 0.8, delay: 0.4 }}
         >
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d252230.0202897456!2d13.062233!3d-8.838333!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1a51f15cdc8d2c7d%3A0x850c1c6d0000000!2sLuanda%2C%20Angola!5e0!3m2!1spt-BR!2sao!4v1710000000000!5m2!1spt-BR!2sao"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d105291.77111161467!2d13.20170168145374!3d-8.853353711556109!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1a51f15cdc8d2c7d%3A0x850c1c5c5ecc5a92!2sLuanda!5e1!3m2!1spt-PT!2sao!4v1775821804479!5m2!1spt-PT!2sao"
             className="w-full h-full border-0 grayscale hover:grayscale-0 transition-all duration-1000"
             loading="lazy"
             allowFullScreen
